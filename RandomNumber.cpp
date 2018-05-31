a5652847c86bc172a9195690088c529961ec6b6b
DiyeMark <30835838+DiyeMark@users.noreply.github.com>
2018-05-31 20:01:11 +0300

    Add files via upload

---
+++ RandomNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "RandomNumber.h"
+
+using namespace std;
+
+int main(){
+	int numb = 	getRandomNumber();
+	cout << numb << endl;
+	
+	system("pause");
+	return 0;	
+}
+
